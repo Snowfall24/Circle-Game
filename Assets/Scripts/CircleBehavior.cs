c2a7aba5b6a1f377b17f6e28f86e5b883352146b
Kyler Smith <kylersmith7@gmail.com>
2016-02-27 12:09:00 -0800

    Circle gets smaller when you click it

--- Assets/Scripts/CircleBehavior.cs
+++ Assets/Scripts/CircleBehavior.cs
@@ -46,7 +46,6 @@ public class CircleBehavior : MonoBehaviour
 
 			SetColor();
 			outlineSize = 3f;
-			size = 1.5f;
             delay = maxdelay;
 
 			//Add text
@@ -55,6 +54,13 @@ public class CircleBehavior : MonoBehaviour
 			GameObject t = Instantiate(text, mousePos, Quaternion.identity) as GameObject;
             t.GetComponent<TextMesh>().text = texts[Random.Range(0, texts.Count - 1)];
         }
+		if (Input.GetMouseButton(0)) {
+			size = 1.8f;
+		} else {
+			size = 2f;
+		}
+
+		SR.transform.localScale = Vector3.Lerp(SR.transform.localScale, new Vector3(size, size, 1f), Time.deltaTime * 10);
 		SR.color = Color.Lerp(SR.color, color, Time.deltaTime * 10f);
 
 		Outline();
