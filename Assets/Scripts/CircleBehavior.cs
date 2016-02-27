7e244cf36258d7f7694dc23271a8816cdc8f7d19
Kyler Smith <kylersmith7@gmail.com>
2016-02-27 14:05:08 -0800

    Fix text not showing bug

--- Assets/Scripts/CircleBehavior.cs
+++ Assets/Scripts/CircleBehavior.cs
@@ -4,62 +4,59 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
-public class CircleBehavior : MonoBehaviour
-{
+public class CircleBehavior : MonoBehaviour {
 	public SpriteRenderer outline;
 	float outlineSize = 2f;
 	float size = 2f;
 
 	public AudioSource clickSound;
 
-    SpriteRenderer SR;
-    List<string> texts = new List<string>();
-    GameObject text;
+	SpriteRenderer SR;
+	//List<string> texts = new List<string>();
+	string[] texts;
+	GameObject text;
 	GameObject particles;
 
 	Color color;
 
+	TextAsset file;
 
+	float delay = 0;
+	float maxdelay = 0.1f;
 
-    float delay = 0;
-    float maxdelay = 0.1f;
+	// Use this for initialization
+	void Start() {
+		SR = GetComponent<SpriteRenderer>();
 
-    // Use this for initialization
-    void Start()
-    {
-        SR = GetComponent<SpriteRenderer>();
-        StreamReader sr = new StreamReader(Application.dataPath + "\\circlegametext.txt");
-        while (!sr.EndOfStream)
-        {
-            texts.Add(sr.ReadLine());
-        }
+		file = Resources.Load("circlegametext") as TextAsset;
+		texts = file.text.Split(new string[] { System.Environment.NewLine }, System.StringSplitOptions.None);
 
-        text = Resources.Load("Text") as GameObject;
+		text = Resources.Load("Text") as GameObject;
 		particles = Resources.Load("Particles") as GameObject;
 
 		SetColor();
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        delay -= Time.deltaTime;
+	// Update is called once per frame
+	void Update() {
+		delay -= Time.deltaTime;
 
-        if ((Input.GetMouseButtonDown(0) && delay <= 0) || (Input.GetMouseButton(1) && delay <= -0.1f))
-        {
+		if ((Input.GetMouseButtonDown(0) && delay <= 0) || (Input.GetMouseButton(1) && delay <= -0.1f)) {
 			clickSound.Play();
 
 			SetColor();
 			outlineSize = 3f;
-            delay = maxdelay;
+			delay = maxdelay;
 
 			//Add text
 			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			mousePos.z = -1;
 			GameObject t = Instantiate(text, mousePos, Quaternion.identity) as GameObject;
 			Instantiate(particles, mousePos, Quaternion.Euler(0, 180, 180));
-            t.GetComponent<TextMesh>().text = texts[Random.Range(0, texts.Count - 1)];
-        }
+			t.GetComponent<TextMesh>().text = texts[Random.Range(0, texts.Length - 1)];
+
+			//Debug.Log("Text created at: " + mousePos.x + " " + mousePos.y + " " + mousePos.z);
+		}
 		if (Input.GetMouseButton(0) || Input.GetMouseButton(1)) {
 			size = 1.8f;
 		} else {
@@ -74,7 +71,7 @@ public class CircleBehavior : MonoBehaviour
 		if (Input.GetKey(KeyCode.Escape)) {
 			SceneManager.LoadScene(0);
 		}
-    }
+	}
 
 	void SetColor() {
 		color = Color.HSVToRGB(Random.Range(0f, 1f), 1f, 0.9f);
