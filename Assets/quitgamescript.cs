2fe7909252ed16e06945e89ec931d910747972f7
Snowfall24 <omglegobuilder@gmail.com>
2016-02-27 13:28:34 -0800

    Creating quit game function

---
+++ Assets/quitgamescript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class quitgamescript : MonoBehaviour {
+
+    public void QuitGame()
+    {
+        Application.Quit; 
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
