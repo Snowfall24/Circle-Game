5d37f4fe649ceb0fe86b6529f0b378410e3db2c8
Snowfall24 <omglegobuilder@gmail.com>
2016-02-27 12:32:40 -0800

    CHANGES

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+    
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("CircleScene");
+    }
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
