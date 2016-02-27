4499cea988947403aa42bc5159cfc8688f6e6d59
Snowfall24 <omglegobuilder@gmail.com>
2016-02-26 20:15:45 -0800

    Added unity 3d project

---
+++ Assets/CircleBehavior.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class CircleBehavior : MonoBehaviour
+{
+    SpriteRenderer SR;
+    List<string> texts = new List<string>();
+    GameObject text;
+
+
+
+
+    float delay = 0;
+    float maxdelay = 0.1f;
+    // Use this for initialization
+    void Start()
+    {
+        SR = GetComponent<SpriteRenderer>();
+        StreamReader sr = new StreamReader(Application.dataPath + "\\circlegametext.txt");
+        while (!sr.EndOfStream)
+        {
+            texts.Add(sr.ReadLine());
+        }
+
+        Debug.Log(texts.ToString());
+
+        text = Resources.Load("Text") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delay -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && delay <= 0)
+        {
+            SR.color = Color.HSVToRGB(Random.Range(0f, 1f), 1, 1);
+            delay = maxdelay;
+            GameObject t = Instantiate(text) as GameObject;
+            t.GetComponent<TextMesh>().text = texts[Random.Range(0, texts.Count - 1)];
+        }
+    }
+}
---
+++ Assets/Resources/Float.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Float : MonoBehaviour
+{
+    TextMesh txt;
+
+    // Use this for initialization
+    void Start()
+    {
+        txt = GetComponent<TextMesh>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.color = new Color(1, 1, 1, txt.color.a - 0.5f * Time.deltaTime);
+        transform.position += new Vector3(0, 2 * Time.deltaTime, 0);
+        if (txt.color.a <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
